724a7b87fb1b563596241611077777c3144fb9b1
Biplab Das <31479620+Biplab96Das@users.noreply.github.com>
2022-12-03 22:49:39 +0530

    Update READ_DATA.cpp

--- READ_DATA.cpp
+++ READ_DATA.cpp
@@ -4,10 +4,10 @@ int main()
 {
   int age;
   string name;
-  cout<<"Please type your name "<<endl;
-  cin>>name;
+  cout<<"Please type your full name "<<endl;
+  getline(cin,name);
   cout<<"Please type your age "<<endl;
   cin>>age;
-  cout<<"Hello "<<name<<",You're "<<age<<"years old."<<endl;
+  cout<<"Hello "<<name<<",You're "<<age<<" years old."<<endl;
   return 0;
 }
