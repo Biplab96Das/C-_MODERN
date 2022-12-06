e304b55469524623586a010943384e9f7a3e074c
Biplab Das <31479620+Biplab96Das@users.noreply.github.com>
2022-12-06 18:48:01 +0530

    Add files via upload

---
+++ Statement_function.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int sum(int a,int b)
+{
+    return (a+b);
+}
+int main()
+{
+    int f {3},s {5};
+    cout<<"sum="<<f+s<<endl;
+    cout<<sum(25,17)<<endl;
+    return 0;
+}
