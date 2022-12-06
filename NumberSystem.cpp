da53c8f9b9c5a73d21dabc1e533b87ace97ac9e9
Biplab Das <31479620+Biplab96Das@users.noreply.github.com>
2022-12-06 21:20:15 +0530

    Add files via upload

---
+++ NumberSystem.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+//Number System
+int main()
+{
+  int n1=15;//Decimal
+  int n2=017;//Octal
+  int n3=0x0f;//Hexa-Decimal
+  int n4=0b00001111;//Binary-C++14
+  cout<<"Number 1="<<n1<<endl;
+  cout<<"Number 2="<<n2<<endl;
+  cout<<"Number 3="<<n3<<endl;
+  cout<<"Number 4="<<n4<<endl;
+  return 0;  
+}
