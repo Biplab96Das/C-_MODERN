2b7651a5911ffc815bc6b43688ca253c3d53fbfd
Biplab Das <31479620+Biplab96Das@users.noreply.github.com>
2022-12-06 18:26:50 +0530

    Add files via upload

---
+++ comments.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+/*This code depicts my target for Online_Test_prep
+Other then this I have to do OS,DBMS,CN,OOPs,System Desgn,
+Machine Learning project and/or Block-Chain project.
+This is how we can write multi-line comment*/
+using namespace std;
+int main()
+{   cout<<"Subjects are:C++,GFG,DSA_Sheet_Striver,Codechef,CodeForces";//This is one line comment
+    cout<<",LeetCode."<<endl;
+    return 0;
+}
