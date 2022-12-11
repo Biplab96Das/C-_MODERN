103ffbc3f5ab8c1eee5a06a32608eeb69b902a6f
Biplab Das <31479620+Biplab96Das@users.noreply.github.com>
2022-12-11 17:17:42 +0530

    Add files via upload

---
+++ expression.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+   int a {6};
+   int b {3};
+   int c {8};
+   int d {9};
+   int e {3};
+   int f {2};
+   int g {5};
+   int res = a + b * c - d / e -f + g;
+   int exp = a / b * c + d - e + f;
+   cout<<"First expression = a + b * c - d / e -f - g = "<<res<<endl;
+   cout<<"Second expression = a / b * c + d - e + f = "<<exp<<endl;
+   res = ( a + b ) * c - d / e -f + g;
+   cout<<"Third expression =( a + b ) * c - d / e -f + g= "<<res<<endl;
+   return 0;
+   }
