14392d63ac4f808cbed02043ac741b2d1713129b
Biplab Das <31479620+Biplab96Das@users.noreply.github.com>
2022-12-12 16:32:58 +0530

    Add files via upload

---
+++ Compound_Assign.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{  int value {5};
+   cout<<"-------Compound Assignment-------"<<endl;
+   cout<<"value initially= "<<value<<endl;
+   value +=4;
+   cout<<"value+=4;value= "<<value<<endl;
+   value -=3;
+   cout<<"value-=3;value= "<<value<<endl;
+   value *=6;
+   cout<<"value*=6;value= "<<value<<endl;
+   value /=3;
+   cout<<"value/=3;value= "<<value<<endl;
+   value%=5;
+   cout<<"value%=5;value= "<<value<<endl;
+   return 0;
+   }
