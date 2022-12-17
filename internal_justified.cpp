53df3297cb574b017efea501829c92991fb0211e
Biplab Das <31479620+Biplab96Das@users.noreply.github.com>
2022-12-17 20:15:20 +0530

    Add files via upload

---
+++ internal_justified.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+int main()
+{ cout<<endl;
+  cout<<right;
+  cout<<setw(10)<<-123.5<<endl;
+  cout<<internal;
+  cout<<setw(10)<<-123.5<<endl;
+ return 0;  
+}
