d84ce91177246b1b49e5551de70f07bfe8e45111
Biplab Das <31479620+Biplab96Das@users.noreply.github.com>
2022-12-17 20:58:30 +0530

    Add files via upload

--- New_Line.cpp
+++ New_Line.cpp
@@ -2,9 +2,9 @@
 #include<iomanip>
 using namespace std;
 int main()
-{  cout<<"Hello";
-   cout<<"World";
-   cout<<"------------------";
+{  cout<<"Hello"<<flush<<" ";//Flush tranfers input data directly to output without 
+   cout<<"World"<<flush<<endl;//Sending it to the buffer
+   cout<<"------------------"<<endl;
    cout<<"Hello"<<endl;//endl gives new line
    cout<<"World"<<endl;
    cout<<"Hello\n";//\n gives new line
