68cdb20eaa222ee1ec455d29216083df6d0f0367
Biplab Das <31479620+Biplab96Das@users.noreply.github.com>
2022-12-17 20:40:00 +0530

    Add files via upload

---
+++ New_Line.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+int main()
+{  cout<<"Hello";
+   cout<<"World";
+   cout<<"------------------";
+   cout<<"Hello"<<endl;//endl gives new line
+   cout<<"World"<<endl;
+   cout<<"Hello\n";//\n gives new line
+   cout<<"World\n";
+ return 0;  
+}
