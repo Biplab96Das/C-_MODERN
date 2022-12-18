6e36c252b0480bbd8256b2edbdb094566eed6afc
Biplab Das <31479620+Biplab96Das@users.noreply.github.com>
2022-12-18 17:54:59 +0530

    Add files via upload

---
+++ mathf.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+int main()
+{  
+ double a=-7.18634;
+ cout<<floorf(a)<<endl;
+ cout<<ceilf(a)<<endl;
+ cout<<abs(a)<<endl;
+ cout<<cosf(60)<<endl;
+ cout<<sinf(30)<<endl;
+ cout<<tanf(45)<<endl;
+ cout<<exp2f(10)<<endl;
+ cout<<expf(1)<<endl;
+ cout<<logf(expf(1))<<endl;
+ cout<<log10f(10)<<endl;
+ cout<<log2f(2)<<endl;
+ cout<<powf(9,2)<<endl;
+ cout<<sqrtf(100)<<endl;
+ cout<<cbrtf(1000)<<endl;
+ cout<<hypotf(4,3)<<endl;
+ return 0;  
+}
