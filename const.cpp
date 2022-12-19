9b61a75e8a41e81f5d1c37022554706604a879c0
Biplab Das <31479620+Biplab96Das@users.noreply.github.com>
2022-12-19 23:43:23 +0530

    Add files via upload

---
+++ const.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{  //const can only go with "Declare and Initialize"
+    const unsigned int earth_radius_km {6371};//in km
+    const unsigned int earth_radius_km_1 {6371};//in km
+    const unsigned int earth_radius_km_2 {6371u};//in km
+  //Modifying variable of const type is not possible
+    //earth_radius_km=771;//Compilation error
+    cout<<"Earth's radius = "<<earth_radius_km<<endl;
+    int expanded_radius {3*earth_radius_km};
+    cout<<"Three times of earth's radius = "<<expanded_radius<<endl;
+ return 0;   
+}
