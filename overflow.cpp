a92e7e384d31731718cf21b4aba4110a778fe159
Biplab Das <31479620+Biplab96Das@users.noreply.github.com>
2022-12-22 18:31:25 +0530

    Add files via upload

---
+++ overflow.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+ unsigned char char_val {55};
+ unsigned char val1 {130};
+ unsigned char val2 {131};
+ char_val = val1 + val2;//char_val store spade of card
+ cout<<"Char value is = "<<char_val<<endl;
+return 0;
+}
