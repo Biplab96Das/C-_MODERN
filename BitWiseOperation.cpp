09140d3f402a9d69a483d73377b6cbf1c03a3052
Biplab Das <31479620+Biplab96Das@users.noreply.github.com>
2022-12-24 10:46:28 +0530

    Add files via upload

---
+++ BitWiseOperation.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<iomanip>
+#include<bitset>
+using namespace std;
+int main()
+{ 
+  int col_width {20};
+  unsigned short int val1 {0x3};
+  unsigned short int val2 {0x5};
+  cout<<left;//Left justified,setfill(' ') is same as default,giving blank
+  //1 byte bit-wise representation of value 1 and value 2
+  cout<<setw(col_width)<<"value1 is = "<<setw(col_width)<<bitset<8>(val1)<<endl;
+  cout<<setw(col_width)<<"value2 is = "<<setw(col_width)<<bitset<8>(val2)<<endl;
+  //Bit-wise AND operator
+  cout<<setw(col_width)<<"value1&value2 is ="<<setw(col_width)<<bitset<8>(val1 & val2)<<endl;
+  //Bit-wise OR operator
+  cout<<setw(col_width)<<"value1|value2 is ="<<setw(col_width)<<bitset<8>(val1 | val2)<<endl;
+  //Bit-wise NOT operator
+  cout<<setw(col_width)<<"~value1 is = "<<setw(col_width)<<bitset<8>(~val1)<<endl;
+  //Bit-wise XOR operator
+  cout<<setw(col_width)<<"value1^value2 is ="<<setw(col_width)<<bitset<8>(val1 ^ val2)<<endl;
+
+return 0;
+}
