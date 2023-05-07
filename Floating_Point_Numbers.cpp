d9146582a961a6b9b986b82d13e318a54c41ae6e
Biplab Das <31479620+Biplab96Das@users.noreply.github.com>
2023-05-07 15:45:41 +0530

    Update Floating_Point_Numbers.cpp

--- Floating_Point_Numbers.cpp
+++ Floating_Point_Numbers.cpp
@@ -1,11 +1,29 @@
-#include <iostream>
-
-consteval int get_value(){
-    return 3;
-}
-
-int main(){
-    constexpr int value = get_value();
-    std::cout << "value : " << value << std::endl;
-    return 0;
+#include<iostream>
+#include<iomanip>
+using namespace std;
+int main()
+{
+  //Declare and initialize the variables
+	float number1 {1.1234567890F};//7-bit is correct
+	double number2 {1.12345678901234527890};//15
+	long double number3 {1.12345678901234567890l};
+	float number(192400023);//7-bit precision
+	cout<<setprecision(20);
+	cout<<"Number1 is "<<number1<<endl;
+	cout<<"Number2 is "<<number2<<endl;
+	cout<<"Number3 is "<<number3<<endl;
+	cout<<"Number is "<<number<<endl;
+	//Scientific Notation
+	double num5 {192400023};
+	double num6 {1.92400023e8};
+	long double num7 {0.00000003498L};
+	long double num8 {3498e-11L};
+	cout<<num5<<" "<<num6<<" "<<num7<<" "<<num8<<endl;
+	//NaN or Infinity(+/-)
+    double num9 {8.5};
+    double num10 {};//Initialized to 0
+    double num11 {};
+    cout<<num9<<"/"<<num10<<"="<<num9/num10<<endl;//inf
+    cout<<num10<<"/"<<num11<<"="<<num10/num11<<endl;//nan
+	return 0;
 }
