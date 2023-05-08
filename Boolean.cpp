7ef9c77c0404d86f1a1efe0e4dd652c7e4e28cbc
Biplab Das <31479620+Biplab96Das@users.noreply.github.com>
2023-05-08 14:34:27 +0530

    Add files via upload

--- Boolean.cpp
+++ Boolean.cpp
@@ -1,21 +1,18 @@
 #include<iostream>
+#include<iomanip>
 using namespace std;
 int main()
 {
-    bool red_light {true};
-    bool green_light {false};
-    cout<<green_light<<endl;    
-    cout<<red_light<<endl; 
-    if(green_light==true)
-       cout<<"Go through "<<endl;
-    else 
-       cout<<"Green light is not on yet,so wait "<<endl;
-    if (red_light==true)
-       cout<<"Stop! "<<endl;
-    else 
-       cout<<"Go through "<<endl;
-    boolalpha;
-    cout<<green_light<<endl;    
-    cout<<red_light<<endl;  
+    bool green_light{false},red_light{true};
+    if(red_light)
+       cout<<"Stop!";
+    else
+       cout<<"Go through!";
+    if(green_light)
+       cout<<"You can go.";
+    else
+       cout<<"You cann't go."; 
+    cout<<"\nRed_light: "<<red_light<<endl;
+    cout<<boolalpha<<"Red_light: "<<red_light<<endl;        
   return 0;
 }
